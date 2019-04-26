14ab61c78bcb3367097602e07f2e5be91a508c6f
Brian <johnson8727@hotmail.com>
2019-04-25 22:07:51 -0400

    tried to add a caption and call the function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 lightbox.options({
       'resizeDuration': 200,
-      'alwaysShowNavOnTouchDevices': true,
-      'wrapAround': true
+      'alwaysShowNavOnTouchDevices': false,
+      'wrapAround': true,
+      'albumLabel': "Hay Bales I love hay bales. Took this snap on a drive through the countryside past some straw fields."
     })
+
+lightbox.options(); // is this right??
