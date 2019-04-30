3d2c6845bdc50ab78edb96f821a2756e3a6bb4c0
Brian <johnson8727@hotmail.com>
2019-04-29 22:11:25 -0400

    added captions to html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 lightbox.options({
       'resizeDuration': 200,
       'alwaysShowNavOnTouchDevices': false,
-      'wrapAround': true,
-      'albumLabel': "Hay Bales I love hay bales. Took this snap on a drive through the countryside past some straw fields."
+      'wrapAround': true
     })
 
 lightbox.options(); // is this right??
