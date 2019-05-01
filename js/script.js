ccee0b9d9e9aaba3bd2884503af447e87d9bad31
Brian <johnson8727@hotmail.com>
2019-05-01 15:07:18 -0400

    worked on JS search field code

--- js/script.js
+++ js/script.js
@@ -5,5 +5,25 @@ lightbox.option({
     'showImageNumberLabel' : false
   })
 
-  lightbox.option();
-$('mygallery').hide();
+  lightbox.option()
+
+
+ function searchField (){
+//create a variable to hold the value of the search field and change the input to lowercase
+  let inputField = document.getElementById('search').value.toLowerCase();
+//create a variable that stores the anchor tags for the images
+   let aTag = document.querySelector('.photo_container a')
+// create a loop that iterates theu each data-title caption values.
+   for (var i = 0; i < aTag.length; i++ ){
+     let imgCap = aTag[i].getAttribute("data-title").toLowerCase();
+     let match = imgCap.includes(inputField);
+//Then program compares what is typed into search field to what is in captions and filters images
+     if (match){
+       aTag[i].style.display = "";
+     }else{
+      aTag[i].style.display = none;
+     }
+   }
+ }
+
+ searchField();
