a5d0645d179caece88a59e099b98d233d87cbd93
Brian Johnson <johnson8727@hotmail.com>
2020-02-12 23:30:13 -0500

    refactored and with new layout

--- js/script.js
+++ js/script.js
@@ -3,25 +3,21 @@ lightbox.option({
     'wrapAround': true,
     'alwaysShowNavOnTouchDevices': true,
     'showImageNumberLabel' : false
-  })
+  });
 
-  lightbox.option()
+  lightbox.option();
+  
+/*=-=-=-=-=-SEARCH FIELD FUNCTIONALITY=-=-=-=-=-*/
+ document.querySelector('input').addEventListener('keyup', ()=>{
+   //Selecting value from the search field and all the "a" elements
+   const inputField = document.getElementById('search').value.toLowerCase(); 
+   const aTag = document.querySelectorAll('#my_photos a');
+   aTag.forEach(element => {
+    // search filter logic
+    var imgCap = element.getAttribute("data-title").toLowerCase();
+    var match = imgCap.includes(inputField);
+    (match) ? element.style.display = "" : element.style.display = "none";  
+    });  
+   });
 
 
- function searchField (){
-//create a variable to hold the value of the search field and change the input to lowercase
-  var inputField = document.getElementById('search').value.toLowerCase();
-//create a variable that stores the anchor tags for the images
-   var aTag = document.querySelectorAll('#my_photos a');
-// create a loop that iterates thru each data-title caption values.
-   for (var i = 0; i < aTag.length; i++ ){
-     var imgCap = aTag[i].getAttribute("data-title").toLowerCase();
-     var match = imgCap.includes(inputField);
-//Then program compares what is typed into search field to what is in captions and filters images
-     if (match){
-       aTag[i].style.display = "";
-     }else{
-      aTag[i].style.display = "none";
-     }
-   }
- }
